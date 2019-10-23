1caf0864b69f9c1fa0f21fc5b04ffade729354e8
Hoang-Hue <54617666+Hoang-Hue@users.noreply.github.com>
2019-10-23 11:05:04 +0700

    Add files via upload

---
+++ excellent-master/Libraly/js/scripts.js
@@ -0,0 +1,7 @@
+// Empty JS for your own code to be here
+$( document ).ready(function(){
+    $('#menu-trigger').click(function(){
+        $('#list').toggle();
+    })
+    $('.button-collapse').dropdown();
+    });
